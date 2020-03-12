27def7e103fcc473f1760105f82173b9e4209ff4
João Pedro da Silva <60113442+QuodSumEriss@users.noreply.github.com>
2020-03-12 20:58:04 -0300

    Add files via upload

---
+++ Masc_Fem.py
@@ -0,0 +1,8 @@
+sexo = str(input('Digite (M)-Masculino (F)-Feminino: ')).upper()
+
+if sexo == 'M':
+    print('O sexo é Masculino')
+elif sexo == 'F':
+    print('O sexo é Feminino:')
+else:
+    print(('Sexo invalido'))
