bebcf3dc94a08f3e0d4abff3c5286ff69c032b5b
João Pedro da Silva <60113442+QuodSumEriss@users.noreply.github.com>
2020-03-19 17:24:03 -0300

    Add files via upload

---
+++ Turno_Estudo.py
@@ -0,0 +1,14 @@
+"""
+Faça um Programa que pergunte em que turno você estuda. Peça para digitar M-matutino ou V-Vespertino ou N- Noturno. Imprima a mensagem "Bom Dia!", "Boa Tarde!" ou "Boa Noite!" ou "Valor Inválido!", conforme o caso.
+"""
+
+turno = str(input('Qual é o seu horario de estudo? Digite (M)-matutino, (V)-vespertino ou (N)-noturno: ')).upper()
+
+if turno == 'M':
+    print('Bom dia!')
+elif turno == 'V':
+    print('Boa tarde!')
+elif turno == 'N':
+    print('Boa noite!')
+else:
+    print('Valor invalido')
