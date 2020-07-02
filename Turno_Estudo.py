4b51a90587e6a1a88793275fd77c54950573249e
QuodSumEriss <joaopedro.silva.paulino@gmail.com>
2020-07-02 00:32:31 -0300

    Programa para descobrir o turno em que o aluno estuda

--- Turno_Estudo.py
+++ Turno_Estudo.py
@@ -1,5 +1,6 @@
 """
-Faça um Programa que pergunte em que turno você estuda. Peça para digitar M-matutino ou V-Vespertino ou N- Noturno. Imprima a mensagem "Bom Dia!", "Boa Tarde!" ou "Boa Noite!" ou "Valor Inválido!", conforme o caso.
+Faça um Programa que pergunte em que turno você estuda. Peça para digitar M-matutino ou V-Vespertino ou N- Noturno.
+Imprima a mensagem "Bom Dia!", "Boa Tarde!" ou "Boa Noite!" ou "Valor Inválido!", conforme o caso.
 """
 
 turno = str(input('Qual é o seu horario de estudo? Digite (M)-matutino, (V)-vespertino ou (N)-noturno: ')).upper()
