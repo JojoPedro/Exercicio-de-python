3a5bca2a51260b9f293f8ccc94d7e8cc8eb4edc8
QuodSumEriss <joaopedro.silva.paulino@gmail.com>
2020-07-15 22:02:42 -0300

    Programa para uma loja de tintas

--- lojatinta.py
+++ lojatinta.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 """
 Faça um programa para uma loja de tintas. O programa deverá pedir o tamanho em metros quadrados da área a
 ser pintada. Considere que a cobertura da tinta é de 1 litro para cada 3 metros quadrados e que a tinta é vendida
