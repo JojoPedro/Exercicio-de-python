60fe34312cef88c3d125d7052d958b710c93cd75
QuodSumEriss <joaopedro.silva.paulino@gmail.com>
2020-07-19 17:17:31 -0300

    programa que indica o tipo do triangulo

--- triangulo.py
+++ triangulo.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 """
 Faça um Programa que peça os 3 lados de um triângulo. O programa deverá informar se os valores podem ser um triângulo.
 Indique, caso os lados formem um triângulo, se o mesmo é: equilátero, isósceles ou escaleno.
