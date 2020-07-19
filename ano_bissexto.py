2c4e34dbca8be7a820f0a1b1b9b07327c122b6ef
João Pedro da Silva <60113442+QuodSumEriss@users.noreply.github.com>
2020-07-19 17:13:41 -0300

    Update ano_bissexto.py
    Programa que indica se o ano inserido é bissexto ou não

--- ano_bissexto.py
+++ ano_bissexto.py
@@ -2,7 +2,7 @@
 Faça um Programa que peça um número correspondente a um determinado ano e em seguida informe se este ano é ou não bissexto.
 """
 
-ano = int(input('digite um ano (XXXX) : '))
+ano = int(input('Digite um ano (XXXX) : '))
 if (ano%4 == 0 and ano%100 != 0) or ano%400 == 0:
     print('O ano', ano, 'é bissexto')
 else:
